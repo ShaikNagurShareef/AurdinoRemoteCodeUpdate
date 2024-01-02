88a28590e5575e60ea9e9aab372adc2319784328
Shaik Nagur Shareef <45702719+Shareef0612@users.noreply.github.com>
2024-01-01 22:02:33 -0500

    Update starter.py

--- starter.py
+++ starter.py
@@ -3,23 +3,23 @@ from utils import ssh_connect, compare_and_update, get_fqbn_and_port, create_and
 
 try:
     # source file path
-    source_file = "/Users/nagurshareefshaik/Desktop/MSCS/MORSE Studio - GRA/RemoteCodeUpdate/updated_code.ino"
+    source_file = "###############/updated_code.ino"
 
     # target file path
-    target_file = "/Users/nagurshareefshaik/Desktop/MSCS/MORSE Studio - GRA/RemoteCodeUpdate/arduino_code.ino"
+    target_file = "##############/arduino_code.ino"
 
     # Destination server details
-    host = "Tejas-Air.wifi.gsu.edu" #"10.0.0.78" #"Tejas-Air.wifi.gsu.edu"
-    username = "tejacherukuri"
-    private_key = "/Users/nagurshareefshaik/.ssh/id_arduino_teja"
+    host = "################" #"10.0.0.78"
+    username = "###############"
+    private_key = "###############"
 
     # passphrase to decrypt the private key
-    passphrase = "ghs@195L" #getpass.getpass("Enter the passphrase for your private key: ")
+    passphrase = "########" #getpass.getpass("Enter the passphrase for your private key: ")
 
     # file transfer details
-    code_loc = "/Users/tejacherukuri/Shareef/RemoteCodeUpdate/"
-    remote_directory = "/Users/tejacherukuri/Shareef/CodeFiles/"
-    local_file = "/Users/nagurshareefshaik/Desktop/MSCS/MORSE Studio - GRA/RemoteCodeUpdate/arduino_code.ino" # same as target file
+    code_loc = "############" # Remote code location
+    remote_directory = "############" # Remote code directory
+    local_file = "#########/arduino_code.ino" # same as target file
 
     # compare and check if the target file is updated
     update_flag = compare_and_update(source_file, target_file)
